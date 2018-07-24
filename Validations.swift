14fe024b8975e31dc1bd184fbc95c5e4e09a6165
Romil <shadybond@Romils-MacBook-Pro.local>
2018-07-24 00:57:18 -0400

    Add EMail, Password and Name Validation

--- Validations.swift
+++ Validations.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+func isValidPassword(password: String) -> Bool {
+    if(password.count>7){
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+func isValidEmail(testStr:String) -> Bool {
+    let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    
+    let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+    return emailTest.evaluate(with: testStr)
+}
+
+func isValidName(name: String) -> Bool {
+    if(name.count>0){
+        return true
+    } else{
+        return false
+    }
+}
+
+
